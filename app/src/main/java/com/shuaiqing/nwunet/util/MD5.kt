698526e65c45c6e3f5d1b745b83dff06d063caeb
shiqingk <978964112@qq.com>
2017-12-17 16:24:55 +0800

    更新MD5验证。

--- app/src/main/java/com/shuaiqing/nwunet/util/MD5.kt
+++ app/src/main/java/com/shuaiqing/nwunet/util/MD5.kt
@@ -1,7 +1,39 @@
 package com.shuaiqing.nwunet.util
 
+import java.security.MessageDigest
+
 /**
  * Created by shuaiqing on 2017/12/17.
  */
+
 object MD5 {
+    /**
+     * MD5 加密并返回十六进制结果
+     */
+    fun md5(text: String): String {
+        val messageDigest = MessageDigest.getInstance("MD5")
+        messageDigest.update(text.toByteArray())
+        val bytes = messageDigest.digest()
+        return parseHex(bytes)
+    }
+
+    private fun parse(bytes: ByteArray): String {
+        val sb = StringBuilder()
+        for (b in bytes) {
+            sb.append(b)
+        }
+        return sb.toString()
+    }
+
+    private fun parseHex(bytes: ByteArray): String {
+        val sb = StringBuilder()
+        for (b in bytes) {
+            val tmp = (b.toInt() and 0xff).toString(16)
+            if (tmp.length == 1) {
+                sb.append("0")
+            }
+            sb.append(tmp)
+        }
+        return sb.toString()
+    }
 }
